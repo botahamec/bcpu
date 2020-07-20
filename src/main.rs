eef6e996e02dbf965b969fa7e5f86c402af8dbe6
Botahamec <botahamec@outlook.com>
2020-07-20 13:55:52 -0400

    Reads file name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+
+	// read file name
+	let file_name = match std::env::args().next() {
+		Some(n) => n,
+		None => {
+			let mut input = String::new();
+			print!("Enter a bCPU executable filename: ");
+			std::io::stdin().read_line(&mut input).expect("Unable to read input");
+
+			input
+		}
+	};
 }
